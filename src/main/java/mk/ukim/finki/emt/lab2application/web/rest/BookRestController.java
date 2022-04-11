06564584958412d01a687d9cde97fff973154781
Zagorka <jovanovazagorka@outlook.com>
2022-04-12 00:27:59 +0200

    change @CrossOrigin link

--- src/main/java/mk/ukim/finki/emt/lab2application/web/rest/BookRestController.java
+++ src/main/java/mk/ukim/finki/emt/lab2application/web/rest/BookRestController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://emtlibrary-frontend.herokuapp.com/")
 @RequestMapping("/api")
 public class BookRestController {
     private final BookService bookService;
